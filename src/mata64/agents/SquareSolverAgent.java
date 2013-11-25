2d1204c5b87568b6a0ae77583bb82f1cfeda0e7c
Otávio Ribeiro Filho <otavio.r.filho@gmail.com>
2013-11-25 17:30:29 -0300

    Criação da primeira classe

---
+++ src/mata64/agents/SquareSolverAgent.java
@@ -0,0 +1,11 @@
+/*
+ * Class that contains the agent which is responsible for the square game solving
+ */
+
+package mata64.agents;
+
+import jade.core.*;
+
+public class SquareSolverAgent extends Agent {
+
+}
